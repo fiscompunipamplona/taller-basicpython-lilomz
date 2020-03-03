1f650bbddb523708f3b6498b4e256535a70d1c91
monicarico-210 <yadirarico210@gmail.com>
2020-03-03 13:58:42 -0500

    trabajo clase 3marzo20

---
+++ oper.py
@@ -0,0 +1,33 @@
+#Linda Moreno
+#Ninibed Gonzalez
+
+
+a=6
+b=15
+c=6.3
+d=15.45
+e=4+15j
+f=6j
+
+print( a+b, b*a, a/b, b-a, b**a)
+print(c+d, d-c, c*d, d/c, c**d)
+print(e+f, f-e, e*f)
+print(a+f)
+
+
+print(a,b,sep="...")
+print(a,b,sep="")
+print(a,"\n",b,"\n",c)
+
+x=input("entre valor x:")
+
+print(3*x)
+
+
+
+
+g=-9.8
+t=float(input("t="))
+h=float(input("h"))
+y=(0.5*g*(t**2))+h
+print(y)
